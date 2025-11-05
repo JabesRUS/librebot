1dd1b9c0330412f011fe0a241fc19874e55de4ce
JabesRus_Asus <jabesgames@gmail.com>
2025-11-05 14:32:06 +0300

    init commit

---
+++ src/main/java/com/jabes/librebot/LibrebotApplication.java
@@ -0,0 +1,18 @@
+package com.jabes.librebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrebotApplication {
+
+    /**
+     * Точка входа в приложение.
+     *
+     * @param args аргументы командной строки
+     */
+	public static void main(String[] args) {
+		SpringApplication.run(LibrebotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jabes/librebot/LibrebotApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jabes.librebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+        // TODO document why this method is empty
+    }
+
+}
